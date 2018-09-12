ebc85aee232b421e187805639be07665b392bd4c
Martin H. J. Bramwell <martinhbramwell@gmail.com>
2018-09-12 13:04:54 -0400

    Enhance (npm): Initial skeleton
    Signed-off-by: Martin H. J. Bramwell <martinhbramwell@gmail.com>

---
+++ packages/usage/index.js
@@ -0,0 +1,7 @@
+var client = require('client');
+var database = require('database');
+var lambdaAuth = require('lambdaAuth');
+var lambdaSrv = require('lambdaSrv');
+
+console.log(client + " " + database + " " + lambdaAuth + " " + lambdaSrv)
+
