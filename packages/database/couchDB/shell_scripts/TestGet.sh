77a2d1b138241ff158d0b818867da3866996c342
Martin H. J. Bramwell <martinhbramwell@gmail.com>
2018-11-20 08:36:38 -0500

    Enhance (db): Adds a bash curl script to update a single field in a given record
    Signed-off-by: Martin H. J. Bramwell <martinhbramwell@gmail.com>

--- packages/database/couchDB/shell_scripts/TestGet.sh
+++ packages/database/couchDB/shell_scripts/TestGet.sh
@@ -46,33 +46,57 @@ fi;
 
 # curl -H "Content-type: application/json" "${COUCH_URL}/${COUCH_DATABASE}/${QUERY}?since=0&limit=10";
 
+# ----------------------------------------------------------------------------------------
+# export dataset="bottle";
+# export QUERY="_design/visible/_view/count_${dataset}";
+# echo -e "
+# Getting test query: ${COUCH_URL}/${COUCH_DATABASE}/${QUERY}";
+# echo -e "# of ${dataset} records : $(curl -sH "Content-type: application/json" "${COUCH_URL}/${COUCH_DATABASE}/${QUERY}" | jq -r .rows[0].value)";
 
-export dataset="bottles";
-export QUERY="_design/visible/_view/count_${dataset}";
-echo -e "
-Getting test query: ${COUCH_URL}/${COUCH_DATABASE}/${QUERY}";
-echo -e "# of ${dataset} records : $(curl -sH "Content-type: application/json" "${COUCH_URL}/${COUCH_DATABASE}/${QUERY}" | jq -r .rows[0].value)";
+# export dataset="person";
+# export QUERY="_design/visible/_view/count_${dataset}";
+# echo -e "
+# Getting test query: ${COUCH_URL}/${COUCH_DATABASE}/${QUERY}";
+# echo -e "# of ${dataset} records : $(curl -sH "Content-type: application/json" "${COUCH_URL}/${COUCH_DATABASE}/${QUERY}" | jq -r .rows[0].value)";
 
-export dataset="persons";
-export QUERY="_design/visible/_view/count_${dataset}";
-echo -e "
-Getting test query: ${COUCH_URL}/${COUCH_DATABASE}/${QUERY}";
-echo -e "# of ${dataset} records : $(curl -sH "Content-type: application/json" "${COUCH_URL}/${COUCH_DATABASE}/${QUERY}" | jq -r .rows[0].value)";
+# export dataset="invoice";
+# export QUERY="_design/visible/_view/count_${dataset}";
+# echo -e "
+# Getting test query: ${COUCH_URL}/${COUCH_DATABASE}/${QUERY}";
+# echo -e "# of ${dataset} records : $(curl -sH "Content-type: application/json" "${COUCH_URL}/${COUCH_DATABASE}/${QUERY}" | jq -r .rows[0].value)";
 
-export dataset="products";
-export QUERY="_design/visible/_view/count_${dataset}";
-echo -e "
-Getting test query: ${COUCH_URL}/${COUCH_DATABASE}/${QUERY}";
-echo -e "# of ${dataset} records : $(curl -sH "Content-type: application/json" "${COUCH_URL}/${COUCH_DATABASE}/${QUERY}" | jq -r .rows[0].value)";
+# export dataset="movement";
+# export QUERY="_design/visible/_view/count_${dataset}";
+# echo -e "
+# Getting test query: ${COUCH_URL}/${COUCH_DATABASE}/${QUERY}";
+# echo -e "# of ${dataset} records : $(curl -sH "Content-type: application/json" "${COUCH_URL}/${COUCH_DATABASE}/${QUERY}" | jq -r .rows[0].value)";
 
-export dataset="invoices";
-export QUERY="_design/visible/_view/count_${dataset}";
-echo -e "
-Getting test query: ${COUCH_URL}/${COUCH_DATABASE}/${QUERY}";
-echo -e "# of ${dataset} records : $(curl -sH "Content-type: application/json" "${COUCH_URL}/${COUCH_DATABASE}/${QUERY}" | jq -r .rows[0].value)";
+# export dataset="product";
+# export QUERY="_design/visible/_view/count_${dataset}";
+# echo -e "
+# Getting test query: ${COUCH_URL}/${COUCH_DATABASE}/${QUERY}";
+# echo -e "# of ${dataset} records : $(curl -sH "Content-type: application/json" "${COUCH_URL}/${COUCH_DATABASE}/${QUERY}" | jq -r .rows[0].value)";
 
-export dataset="movements";
-export QUERY="_design/visible/_view/count_${dataset}";
-echo -e "
-Getting test query: ${COUCH_URL}/${COUCH_DATABASE}/${QUERY}";
-echo -e "# of ${dataset} records : $(curl -sH "Content-type: application/json" "${COUCH_URL}/${COUCH_DATABASE}/${QUERY}" | jq -r .rows[0].value)";
+
+
+export QUERY="Product_1_0000000000000002";
+export FULL_URL="${COUCH_URL}/${COUCH_DATABASE}/${QUERY}";
+export FIELD=".*unidad.*";
+export NEW_FIELD="    \"unidad\": \"$(echo $RANDOM % 1000 + 1 | bc) litros\",";
+
+generate_post_data()
+{
+  PRODUCT_2=$(curl -sH "Content-type: application/json" "${FULL_URL}" | jq -r .);
+  echo -e "${PRODUCT_2}" | sed "s/${FIELD}/${NEW_FIELD}/";
+}
+
+# export NEW_PRODUCT_2=$(generate_post_data);
+# echo -e ${NEW_PRODUCT_2};
+
+curl --silent --include \
+  --header "Accept: application/json" \
+  --header "Content-Type:application/json" \
+  --request PUT \
+  --data "$(generate_post_data)" ${FULL_URL}  > /dev/null
+
+curl -sH "Content-type: application/json" "${COUCH_URL}/${COUCH_DATABASE}/${QUERY}"
